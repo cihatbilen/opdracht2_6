7e8c11c9fe5f4fe3aa484e820a46f7f87f55d9e0
makerboomRijnland <makerboom@mborijnland.nl>
2022-01-19 11:49:47 +0100

    slideshowPrevious werkt ook

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let slideIndex = 0;
 
 function slideshowSetup() {
     document.getElementById('slideshow-next').addEventListener('click', slideshowNext);
+    document.getElementById('slideshow-prev').addEventListener('click', slideshowPrevious);
 }
 
 function slideshowNext() {
@@ -18,6 +19,24 @@ function slideshowNext() {
     // als einde, dan slideIndex = 0
     if(slideIndex == slides.length) {
         slideIndex = 0;
+    }
+    
+    // volgende slide zichtbaar maken
+    slides[slideIndex].classList.add('visible');
+}
+
+function slideshowPrevious() {
+    // slides is een array van 5 HTML elementen.
+    let slides = document.getElementsByClassName('slide');
+
+    // zichtbare slide verbergen
+    slides[slideIndex].classList.remove('visible');
+
+    slideIndex -= 1; // slideIndex = slideIndex - 1;
+
+    // als begin, dan slideIndex = 4?
+    if(slideIndex == -1) {
+        slideIndex = slides.length - 1;
     }
     
     // volgende slide zichtbaar maken
